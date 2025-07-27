8a9b055cb8e7700be3710390b97e794048947bf4
hrashid2000 <hamzarashid796@gmail.com>
2025-07-27 18:44:38 -0400

    first commit

---
+++ envs/prod.tfvars
@@ -0,0 +1,22 @@
+#project
+project_id = "seventh-dryad-467217-n4"
+
+#network 
+vpc_name = "default"
+aubnet_name = "default"
+
+#bucket
+bucket_name = "hamza-bucket-prod"
+
+#virtual machine
+vm_name = "hamza-vm-prod"
+machine_type = "e2-small"
+zone = "us-east1-b"
+
+#sql instance
+sql_instance_name = "hamza_postgres_17-prod"
+db_type = "db-f1-micro"
+db_name = ["customer_db","payments_db"]
+db_username = "adminuser"
+secret_name = "adminuser"
+fw_rule = "hamza_fw"
---
+++ variables.tf
@@ -0,0 +1,87 @@
+variable "project_id" {
+  type = string
+  default = "seventh-dryad-467217-n4"
+}
+
+variable "region" {
+  type = string
+  default = "us-east1"
+}
+
+variable "backend_bucket" {
+  type = string
+  default = "terraform-tfstate-hamza"
+}
+
+variable "backend_bucket_prefix" {
+  type = string
+  default = "terraform/state"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "default"
+}
+
+variable "subnet_name" {
+  type = string
+  default = "default"
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+  default = "US"
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "machine_type" {
+ type = string 
+}
+
+variable "zone" {
+ type = string 
+ default = "us-east1-c"
+}
+
+variable "disk_size" {
+ type = number
+ default = 20
+}
+
+variable "sql_instance_name" {
+  type = string
+}
+
+variable "database_version" {
+  type = string
+  default = "POSTGRES_17"
+}
+
+variable "db_type" {
+  type = string
+}
+
+variable "db_name" {
+  description = "multiple db names"
+  type = list(string)
+}
+
+
+variable "db_username" {
+  type = string
+}
+
+variable "secret_name" {
+    type = string 
+}
+
+variable "fw_rule" {
+    type = string 
+}
